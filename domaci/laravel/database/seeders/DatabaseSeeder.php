c5e7f1836461dd898465f153f1437b743876768c
Korisnik <nikola.rusimovic@external.comtrade.com>
2022-08-15 17:40:52 +0200

    kreiran projekat, model, migracije, seederi

---
+++ domaci/laravel/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Project;
+use App\Models\Task;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       Category::truncate();
+       User::truncate();
+       Project::truncate();
+       Task::truncate();
+
+
+
+       Task::factory(5)->create();
+
+
+    }
+}
